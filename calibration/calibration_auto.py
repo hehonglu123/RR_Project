1bec91ac6aa3ff551b2b70906607f5e69b889cf6
hehonglu123 <hehonglu123@126.com>
2020-09-10 00:06:18 -0400

    calibration_temp

--- calibration/calibration_auto.py
+++ calibration/calibration_auto.py
@@ -19,7 +19,7 @@ def calibrate(obj,ref):	#list of [x_c,y_c] and [x_r,y_r]
 	H[:2,:2]=np.dot(u,vh)
 	return H
 
-key="robot_eef"
+key="eef"
 R=[[1,0,0],[0,1,0],[0,0,1]]
 # R=[[0,0,1],[-1,0,0],[0,-1,0]]
 #input robot name
@@ -52,11 +52,9 @@ place_height=robot_yaml['place_height']
 ####subscription
 # cognex_sub=RRN.SubscribeService('rr+tcp://localhost:52222/?service=cognexsim')
 robot_sub=RRN.SubscribeService(url)
-# distance_sub=RRN.SubscribeService('rr+tcp://localhost:25522?service=Environment')
 ####get client object
 # cognex_inst=cognex_sub.GetDefaultClientWait(1)
 robot=robot_sub.GetDefaultClientWait(1)
-# distance_inst=distance_sub.GetDefaultClientWait(1)
 ####get subscription wire
 ##cognex detection wire
 # detection_wire=cognex_sub.SubscribeWire("detection_wire")
@@ -66,7 +64,6 @@ state_w = robot_sub.SubscribeWire("robot_state")
 ####connection fail callback
 # cognex_sub.ClientConnectFailed += connect_failed
 robot_sub.ClientConnectFailed += connect_failed
-# distance_sub.ClientConnectFailed += connect_failed
 
 ##########Initialize robot constants
 robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", robot)
@@ -74,13 +71,6 @@ halt_mode = robot_const["RobotCommandMode"]["halt"]
 jog_mode = robot_const["RobotCommandMode"]["jog"]	
 position_mode = robot_const["RobotCommandMode"]["position_command"]
 robot.command_mode = halt_mode
-
-##########Connect to Cognex wire
-# ##########Initialize velocity control parameters
-# RobotJointCommand = RRN.GetStructureType("com.robotraconteur.robotics.robot.RobotJointCommand",robot)
-
-robot.command_mode = jog_mode 
-
 
 ##########Initialize robot parameters	#need modify
 num_joints=len(robot.robot_info.joint_info)
@@ -90,12 +80,12 @@ H=np.transpose(np.array(robot.robot_info.chains[0].H.tolist()))
 # joint_type = robot.robot_info.joint_info.joint_type.tolist()
 robot_def=Robot(H,np.transpose(P),np.zeros(num_joints))
 
-# transformations=distance_inst.transformations
-# H_robot=transformations[robot_name].H.reshape((transformations[robot_name].row,transformations[robot_name].column))
 
 #######move to start point
-robot.jog_joint(inv.inv([0.5,-0.3,0.2],R).reshape((num_joints,1)), np.ones((num_joints,)), False, True)
 print("moving to start point")
+robot.command_mode = jog_mode 
+robot.jog_joint(inv.inv([0.5,-0.3,0.2],R).reshape((num_joints,1)), np.ones((num_joints,)), False, True)
+
 
 #initialize coordinate list
 # robot_eef_coordinates=[[robot_state.InValue.kin_chain_tcp['position']['x'][0],robot_state.InValue.kin_chain_tcp['position']['y'][0]]]
@@ -105,7 +95,7 @@ print("moving to start point")
 robot.command_mode = halt_mode
 robot.command_mode = position_mode 
 vel_ctrl = EmulatedVelocityControl(robot,state_w, cmd_w, 0.01)
-###
+
 now=time.time()
 while time.time()-now<5:
 	plan(robot,robot_def,[np.sin(time.time()-now),0.1,0.2],R, vel_ctrl)
