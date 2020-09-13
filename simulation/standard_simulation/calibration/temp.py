a2ae07fc68140cd7b219db4639a1f8b7f6bd9aa9
hehonglu123 <hehonglu123@126.com>
2020-09-13 02:19:07 -0400

    a

--- simulation/standard_simulation/calibration/temp.py
+++ simulation/standard_simulation/calibration/temp.py
@@ -1,4 +1,17 @@
+from scipy.optimize import leastsq
+import numpy as np
 import yaml
-with open('camera.yaml') as file:
-	cam=yaml.load(file)['cam_coordinates']
-print(cam)
+
+
+
+def calc_error(robot,cam,theta):
+	R=np.array([[np.cos(theta)[0],-np.sin(theta)[0]],[np.sin(theta)[0],np.cos(theta)[0]]])
+	error=np.dot(R,np.transpose(np.array(cam)))-np.transpose(np.array(robot))
+	return np.average(error,axis=0)
+
+with open('robot.yaml') as file:
+	robot = np.array(yaml.load(file)['cam_coordinates:'])
+with open('cam.yaml') as file:
+	cam = np.array(yaml.load(file)['robot_eef_coordinates::'])
+
+print(calc_error(robot,cam,3.14))
