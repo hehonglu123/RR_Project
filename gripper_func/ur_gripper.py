e55b1804a4cb583029cc588bdcad8fc88f7e5215
hehonglu123 <hehonglu123@126.com>
2020-09-18 17:25:00 -0400

    ur gripper

--- gripper_func/ur_gripper.py
+++ gripper_func/ur_gripper.py
@@ -2,11 +2,11 @@
 
 def gripper(robot,on):
 	if on:
-		robot.setf_signal("D04",not on)
-		robot.setf_signal("D05",on)
-		robot.setf_signal("D03",on)
+		robot.setf_signal("D4",not on)
+		robot.setf_signal("D5",on)
+		robot.setf_signal("D3",on)
 	else:
-		robot.setf_signal("D05",on)
-		robot.setf_signal("D03",on)
-		robot.setf_signal("D04",not on)
+		robot.setf_signal("D5",on)
+		robot.setf_signal("D3",on)
+		robot.setf_signal("D4",not on)
 	
