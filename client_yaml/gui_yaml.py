e6f870418a468592fbb3df09656dfaf1ef02f583
hehonglu123 <hehonglu123@126.com>
2020-10-16 20:10:36 -0400

    gripper orientation

--- client_yaml/gui_yaml.py
+++ client_yaml/gui_yaml.py
@@ -152,6 +152,7 @@ calibration_R={}
 obj_namelists={}
 pick_height={}
 place_height={}
+gripper_orientation={}
 tag_position={}
 url={}
 tool_url={}
@@ -177,12 +178,14 @@ for i in range(len(robot_namelist)):
 	Label(top, text="Home Position").grid(row=3,column=2*i)
 	Label(top, text="Calibration Speed").grid(row=4,column=2*i)
 	Label(top, text="Calibration Start").grid(row=5,column=2*i)
-	Label(top, text="Object Names").grid(row=6,column=2*i)
-	Label(top, text="Pick Height").grid(row=7,column=2*i)
-	Label(top, text="Place Height").grid(row=8,column=2*i)
-	Label(top, text="Tag Position").grid(row=9,column=2*i)
-	Label(top, text="Service URL").grid(row=10,column=2*i)
-	Label(top, text="Tool URL").grid(row=11,column=2*i)
+	Label(top, text="Calibration Orientation").grid(row=6,column=2*i)
+	Label(top, text="Object Names").grid(row=7,column=2*i)
+	Label(top, text="Pick Height").grid(row=8,column=2*i)
+	Label(top, text="Place Height").grid(row=9,column=2*i)
+	Label(top, text="Tag Position").grid(row=10,column=2*i)
+	Label(top, text="Gripper Orientation").grid(row=11,column=2*i)
+	Label(top, text="Service URL").grid(row=12,column=2*i)
+	Label(top, text="Tool URL").grid(row=13,column=2*i)
 
 	robot_name[robot_namelist[i]] = Entry(top)
 	robot_command[robot_namelist[i]] = Entry(top)
@@ -195,6 +198,7 @@ for i in range(len(robot_namelist)):
 	pick_height[robot_namelist[i]] = Entry(top)
 	place_height[robot_namelist[i]] = Entry(top)
 	tag_position[robot_namelist[i]] = Entry(top)
+	gripper_orientation[robot_namelist[i]] = Entry(top)
 	url[robot_namelist[i]] = Entry(top)
 	tool_url[robot_namelist[i]] = Entry(top)
 
@@ -209,8 +213,9 @@ for i in range(len(robot_namelist)):
 	pick_height[robot_namelist[i]].grid(row=8, column=2*i+1)
 	place_height[robot_namelist[i]].grid(row=9, column=2*i+1)
 	tag_position[robot_namelist[i]].grid(row=10, column=2*i+1)
-	url[robot_namelist[i]].grid(row=11, column=2*i+1)
-	tool_url[robot_namelist[i]].grid(row=12, column=2*i+1)
+	gripper_orientation[robot_namelist[i]].grid(row=11, column=2*i+1)
+	url[robot_namelist[i]].grid(row=12, column=2*i+1)
+	tool_url[robot_namelist[i]].grid(row=13, column=2*i+1)
 
 	robot_name[robot_namelist[i]].insert(0,robot_namelist[i])
 
@@ -226,6 +231,7 @@ obj_namelists['sawyer'].insert(0,'bt,sp')
 pick_height['sawyer'].insert(0,0.105)
 place_height['sawyer'].insert(0,0.095)
 tag_position['sawyer'].insert(0,-0.045)
+gripper_orientation['sawyer'].insert(0,0)
 url['sawyer'].insert(0,'rr+tcp://[fe80::a2c:1efa:1c07:f043]:58654/?nodeid=8edf99b5-96b5-4b84-9acf-952af15f0918&service=robot')
 tool_url['sawyer'].insert(0,'rr+tcp://[fe80::a2c:1efa:1c07:f043]:50500/?nodeid=df94b724-9656-4d4f-9f8d-ef37930be1b4&service=tool')
 
@@ -239,7 +245,7 @@ obj_namelists['ur'].insert(0,'tp,pf')
 pick_height['ur'].insert(0,0.012)
 place_height['ur'].insert(0,0.015)
 tag_position['ur'].insert(0,-0.075)
-
+gripper_orientation['ur'].insert(0,0)
 url['ur'].insert(0,'rr+tcp://[fe80::76d6:e60f:27f6:1e3e]:58653/?nodeid=55ade648-a8c2-4775-a7ec-645acea83525&service=robot')
 tool_url['ur'].insert(0,'rr+tcp://[fe80::76d6:e60f:27f6:1e3e]:50500/?nodeid=d3e1c218-0017-4541-8cd7-f9c00136934d&service=tool')
 
@@ -254,6 +260,7 @@ obj_namelists['abb'].insert(0,'bt,sp')
 pick_height['abb'].insert(0,0.1)
 place_height['abb'].insert(0,0.1)
 tag_position['abb'].insert(0,-0.05)
+gripper_orientation['abb'].insert(0,float(np.pi/4))
 url['abb'].insert(0,'rr+tcp://[fe80::16ff:3758:dcde:4e15]:58651/?nodeid=16a22280-7458-4ce9-bd4d-29b55782a2e1&service=robot')
 tool_url['abb'].insert(0,'None')
 
@@ -286,29 +293,29 @@ discover['abb']=Button(top,text='Discover abb',command=lambda: discover_service(
 
 
 
-create['sawyer'].grid(row=13,column=0)
-calibrate['sawyer'].grid(row=14,column=0)
-move_away['sawyer'].grid(row=14,column=1)
-plug['sawyer'].grid(row=15,column=0)
-gripper['sawyer'].grid(row=15,column=1)
-discover['sawyer'].grid(row=13,column=1)
-start_job['sawyer'].grid(row=16,column=1)
+create['sawyer'].grid(row=14,column=0)
+calibrate['sawyer'].grid(row=15,column=0)
+move_away['sawyer'].grid(row=15,column=1)
+plug['sawyer'].grid(row=16,column=0)
+gripper['sawyer'].grid(row=16,column=1)
+discover['sawyer'].grid(row=14,column=1)
+start_job['sawyer'].grid(row=17,column=1)
 
-create['ur'].grid(row=13,column=2)
-calibrate['ur'].grid(row=14,column=2)
-move_away['ur'].grid(row=14,column=3)
-plug['ur'].grid(row=15,column=2)
-gripper['ur'].grid(row=15,column=3)
-discover['ur'].grid(row=13,column=3)
-start_job['ur'].grid(row=16,column=3)
+create['ur'].grid(row=14,column=2)
+calibrate['ur'].grid(row=15,column=2)
+move_away['ur'].grid(row=15,column=3)
+plug['ur'].grid(row=16,column=2)
+gripper['ur'].grid(row=16,column=3)
+discover['ur'].grid(row=14,column=3)
+start_job['ur'].grid(row=17,column=3)
 
-create['abb'].grid(row=13,column=4)
-calibrate['abb'].grid(row=14,column=4)
-move_away['abb'].grid(row=14,column=5)
-plug['abb'].grid(row=15,column=4)
-gripper['abb'].grid(row=15,column=5)
-discover['abb'].grid(row=13,column=5)
-start_job['abb'].grid(row=16,column=5)
+create['abb'].grid(row=14,column=4)
+calibrate['abb'].grid(row=15,column=4)
+move_away['abb'].grid(row=15,column=5)
+plug['abb'].grid(row=16,column=4)
+gripper['abb'].grid(row=16,column=5)
+discover['abb'].grid(row=14,column=5)
+start_job['abb'].grid(row=17,column=5)
 
 
  
@@ -344,7 +351,7 @@ robot_sub['sawyer'].ClientConnectFailed+= connect_failed
 
 state_w['sawyer'] = robot_sub['sawyer'].SubscribeWire("robot_state")
 label['sawyer'] = Label(top, fg = "black", justify=LEFT)
-label['sawyer'].grid(row=16,column=0)
+label['sawyer'].grid(row=17,column=0)
 label['sawyer'].config(text="test")
 label['sawyer'].after(500,lambda: update_label('sawyer'))
 
@@ -353,7 +360,7 @@ robot_sub['ur'].ClientConnectFailed+= connect_failed
 
 state_w['ur'] = robot_sub['ur'].SubscribeWire("robot_state")
 label['ur'] = Label(top, fg = "black", justify=LEFT)
-label['ur'].grid(row=16,column=2)
+label['ur'].grid(row=17,column=2)
 label['ur'].config(text="test")
 label['ur'].after(500,lambda: update_label('ur'))
 
@@ -361,7 +368,7 @@ robot_sub['abb']=RRN.SubscribeService(str(url['abb'].get()))
 robot_sub['abb'].ClientConnectFailed+= connect_failed
 state_w['abb'] = robot_sub['abb'].SubscribeWire("robot_state")
 label['abb'] = Label(top, fg = "black", justify=LEFT)
-label['abb'].grid(row=16,column=4)
+label['abb'].grid(row=17,column=4)
 label['abb'].config(text="test")
 label['abb'].after(500,lambda: update_label('abb'))	
 
--- gripper_func/abb_gripper.py
+++ gripper_func/abb_gripper.py
@@ -1,5 +1,5 @@
 #gripper helper function for ABB
 from RobotRaconteur.Client import *
 def gripper(robot,on):
-	robot=RRN.ConnectService('rr+tcp://[fe80::76d6:e60f:27f6:1e3e]:58652?service=ur_robot')
+	robot=RRN.ConnectService('rr+tcp://[fe80::a2c:1efa:1c07:f043]:58654/?nodeid=8edf99b5-96b5-4b84-9acf-952af15f0918&service=robot')
 	robot.setf_signal("DO7",on)
