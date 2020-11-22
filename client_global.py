9c5aee2d2e7afd6065fcd6e4493b57b14c18a9cd
hehonglu123 <hehonglu123@126.com>
2020-11-21 19:48:08 -0500

    gloabl planner

---
+++ client_global.py
@@ -0,0 +1,336 @@
+#!/usr/bin/env python3
+
+#Robot Raconteur Project Robot Client
+#pick up and drop detected objects
+import RobotRaconteur as RR
+RRN=RR.RobotRaconteurNode.s
+import numpy as np
+from importlib import import_module
+import time, traceback, sys, yaml, argparse, copy
+
+#connection failed callback
+def connect_failed(s, client_id, url, err):
+	print ("Client connect failed: " + str(client_id.NodeID) + " url: " + str(url) + " error: " + str(err))
+
+#Accept the names of the webcams and the nodename from command line
+parser = argparse.ArgumentParser(description="RR plug and play client")
+parser.add_argument("--robot-name",type=str,help="List of camera names separated with commas")
+args, _ = parser.parse_known_args()
+
+robot_name=args.robot_name
+
+#read conveyor info
+with open(r'client_yaml/testbed.yaml') as file:
+	testbed_yaml = yaml.load(file, Loader=yaml.FullLoader)
+conveyor_speed=testbed_yaml['conveyor_speed']
+
+
+sys.path.append('toolbox')
+inv = import_module(robot_name+'_ik')
+R_ee = import_module('R_'+robot_name)
+from general_robotics_toolbox import Robot
+#old gripper
+# sys.path.append('gripper_func')
+# gripper = import_module(robot_name+'_gripper')
+
+#########read in yaml file for robot client
+with open(r'client_yaml/client_'+robot_name+'.yaml') as file:
+	robot_yaml = yaml.load(file, Loader=yaml.FullLoader)
+url=robot_yaml['url']
+robot_height=robot_yaml['height']
+home=robot_yaml['home']
+obj_namelists=robot_yaml['obj_namelists']
+pick_height=robot_yaml['pick_height']
+place_height=robot_yaml['place_height']
+robot_command=robot_yaml['robot_command']
+tool_url=robot_yaml['tool_url']
+gripper_orientation=robot_yaml['gripper_orientation']
+tool_length=robot_yaml['tool_length']
+
+####################Start Service and robot setup
+###########Connect to corresponding services, subscription mode
+####subscription
+cognex_sub=RRN.SubscribeService('rr+tcp://[fe80::922f:c9e6:5fe5:51d1]:52222/?nodeid=87518815-d3a3-4e33-a1be-13325da2461f&service=cognex')
+detection_sub=RRN.SubscribeService('rr+tcp://localhost:52222/?service=detection')
+robot_sub=RRN.SubscribeService(url)
+tool_sub=RRN.SubscribeService(tool_url)
+distance_sub=RRN.SubscribeService('rr+tcp://localhost:25522?service=Environment')
+####get client object
+robot=robot_sub.GetDefaultClientWait(1)
+distance_inst=distance_sub.GetDefaultClientWait(1)
+#new gripper
+gripper=tool_sub.GetDefaultClientWait(1)
+
+
+gripper_on=False
+####get subscription wire
+##cognex detection wire
+detection_wire=cognex_sub.SubscribeWire("detection_wire")
+detection_wire_kinect=detection_sub.SubscribeWire("detection_wire")
+##distance report wire
+distance_report_wire=distance_sub.SubscribeWire("distance_report_wire")
+
+##robot wire
+pos_w=robot_sub.SubscribeWire('position_command')
+vel_w=robot_sub.SubscribeWire('velocity_command')
+if robot_command=='position_command':
+	cmd_w = pos_w
+else:
+	cmd_w = vel_w
+state_w = robot_sub.SubscribeWire("robot_state")
+####connection fail callback
+cognex_sub.ClientConnectFailed += connect_failed
+robot_sub.ClientConnectFailed += connect_failed
+distance_sub.ClientConnectFailed += connect_failed
+
+##########Initialize robot constants
+robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", robot)
+JointTrajectoryWaypoint = RRN.GetStructureType("com.robotraconteur.robotics.trajectory.JointTrajectoryWaypoint",robot)
+JointTrajectory = RRN.GetStructureType("com.robotraconteur.robotics.trajectory.JointTrajectory",robot)
+joint_names = [j.joint_identifier.name for j in robot.robot_info.joint_info]
+RobotJointCommand = RRN.GetStructureType("com.robotraconteur.robotics.robot.RobotJointCommand",robot)
+
+halt_mode = robot_const["RobotCommandMode"]["halt"]
+jog_mode = robot_const["RobotCommandMode"]["jog"]
+trajectory_mode = robot_const["RobotCommandMode"]["trajectory"]
+position_mode = robot_const["RobotCommandMode"]["position_command"]
+velocity_mode = robot_const["RobotCommandMode"]["velocity_command"]
+trajectory_mode = robot_const["RobotCommandMode"]["trajectory"]
+
+robot.command_mode = halt_mode
+
+# ##########Initialize robot velocity control mode
+if robot_command=="position_command":
+	from vel_emulate_sub import EmulatedVelocityControl
+	vel_ctrl = EmulatedVelocityControl(robot,state_w, cmd_w)
+else:
+	from vel_ctrl_sub import VelocityControl
+	vel_ctrl = VelocityControl(robot,state_w, cmd_w)
+robot.command_mode = trajectory_mode 
+
+##########Initialize robot parameters
+num_joints=len(robot.robot_info.joint_info)
+P=np.array(robot.robot_info.chains[0].P.tolist())
+length=np.linalg.norm(P[1])+np.linalg.norm(P[2])+np.linalg.norm(P[3])
+P[-1]+=np.array(tool_length)
+H=np.transpose(np.array(robot.robot_info.chains[0].H.tolist()))
+robot_def=Robot(H,np.transpose(P),np.zeros(num_joints))
+
+
+##########load homogeneous transformation parameters Cognex->robot 
+transformations=distance_inst.transformations
+H_robot=transformations[robot_name].H.reshape((transformations[robot_name].row,transformations[robot_name].column))
+
+##########conveyor belt associated parameters
+obj_vel=np.append(np.dot(H_robot[:-1,:-1],np.array([[conveyor_speed],[0]])).flatten(),0)
+
+def exe_traj(traj):
+	if len(traj.waypoints)<=1:
+		return
+
+	traj_gen = robot.execute_trajectory(traj)
+	while (True):
+		try:
+			res = traj_gen.Next()
+		except RR.StopIterationException:
+			distance_inst.clear_traj(robot_name)
+			return
+
+#jog robot joint helper function
+def jog_joint(q):
+	robot.command_mode = halt_mode
+	time.sleep(0.02)
+	robot.command_mode = jog_mode
+	maxv=[1.3]*(num_joints-1)+[3.2]
+	robot.jog_freespace(q, np.array(maxv), True)
+	robot.command_mode = halt_mode
+	time.sleep(0.01)
+	robot.command_mode = trajectory_mode
+	return
+
+
+# #move to a point with planner
+def single_move(p):
+	traj=None
+	while traj is None:
+		try:
+			traj=distance_inst.plan(robot_name,2.,p,list(R_ee.R_ee(0).flatten()),[0.,0.,0.],0)
+
+		except:
+			print("replanning")
+			time.sleep(0.2)
+			pass
+	
+	exe_traj(traj)
+	
+	return
+#threshold angle
+def angle_threshold(angle):
+	if (angle<-np.pi):
+		angle+=2*np.pi
+	elif (angle>np.pi):
+		angle-=2*np.pi
+	return angle
+#convert cognex frame to robot frame
+def conversion(x,y,height):
+	p=np.dot(H_robot,np.array([[x],[y],[1]])).flatten()
+	p[2]=height
+	return p
+
+def pick(obj):	
+	global gripper_on
+
+	#coordinate conversion
+	print("picking "+obj.name)
+	obj_pick_height=pick_height+testbed_yaml[obj.name]
+	p=conversion(obj.x,obj.y,obj_pick_height)							
+	print(obj.name+' at: ',p)
+
+	R=R_ee.R_ee(angle_threshold(np.radians(obj.angle)-gripper_orientation))
+	#move to object above
+	traj=None
+	while traj is None:
+		try:
+			traj=distance_inst.plan(robot_name,2.,[p[0],p[1],p[2]+0.15],list(R_ee.R_ee(0).flatten()),[0.,0.,0.],0)
+		except:
+			print("replanning")
+			time.sleep(0.2)
+			pass
+
+	exe_traj(traj)
+	#move down
+	q=inv.inv(np.array([p[0],p[1],p[2]]),R)
+
+	jog_joint(q)
+
+	time.sleep(0.1)
+
+	#grab it
+	# gripper.gripper(robot,True)
+	gripper.close()
+	gripper_on=True
+	print("get it")
+	q=inv.inv(np.array([p[0],p[1],p[2]+0.15]),R)
+	jog_joint(q)
+	return
+
+def place(obj,slot_name):
+	global gripper_on
+
+	obj_place_height=place_height+testbed_yaml[obj.name]+0.025
+
+	#coordinate conversion
+	print("placing at "+slot_name)
+	wire_packet=detection_wire.TryGetInValue()
+	slot=wire_packet[1][slot_name]
+	capture_time=float(wire_packet[2].seconds)+float(wire_packet[2].nanoseconds*1e-9)
+
+	p=conversion(slot.x,slot.y,obj_place_height)
+
+	print(slot_name+'at: ',p)
+	#get correct orientation
+
+	R=R_ee.R_ee(angle_threshold(np.radians(slot.angle)-gripper_orientation))
+	
+	box_displacement=[[0],[0],[0]]
+
+	traj=None
+	while traj is None:
+		try:
+			traj=distance_inst.plan(robot_name,2.,[p[0],p[1],p[2]+0.15],list(R.flatten()),list(obj_vel.flatten()),capture_time)
+		except:
+			print("replanning")
+			time.sleep(0.2)
+			pass
+	exe_traj(traj)
+
+	jog_joint_time=1.
+	if robot_name=='abb':
+		jog_joint_time=2.
+	box_displacement=obj_vel*(jog_joint_time+time.time()-capture_time)
+	q=inv.inv(np.array([p[0]+box_displacement[0],p[1]+box_displacement[1],p[2]]),R)
+	q[-1]=angle_threshold(q[-1]-vel_ctrl.joint_position()[-1])+vel_ctrl.joint_position()[-1]
+
+	jog_joint(q)
+	time.sleep(0.1)	#avoid inertia
+	print("dropped")
+	# gripper.gripper(robot,False)
+	gripper.open()
+	gripper_on=False
+	
+	q=inv.inv(np.array([p[0]+box_displacement[0],p[1]+box_displacement[1],p[2]+0.13]),R)
+	jog_joint(q)
+	return
+
+
+def main():
+
+	#open gripper
+	# gripper.gripper(robot,False)
+	gripper.open()
+	#go home first
+	single_move(home)
+
+	#wait until wire value set
+	while True:
+		if detection_wire_kinect.TryGetInValue()[0] and detection_wire.TryGetInValue()[0]:
+			print("here")
+			break
+
+
+	obj_grabbed=None
+	action_performed=True
+	remaining_obj=[]
+	while True:
+		#fill in remaining object list
+		if len(remaining_obj)==0:
+			remaining_obj=copy.deepcopy(obj_namelists)
+
+		if action_performed:
+			print('going home')
+			single_move(home)
+			action_performed=False
+
+
+		if not gripper_on:
+
+			for obj_name in remaining_obj:
+				#check current robot free, and pick the object
+				obj=detection_wire_kinect.InValue[obj_name]
+				#pick obj&slot both detected object with priority
+				if obj.detected and detection_wire.InValue[obj.name[0]+'_f'].detected:
+					pick(obj)
+					obj_grabbed=obj
+					action_performed=True
+					remaining_obj.remove(obj_name)
+					break
+			#if no slots detected, pick up available object first
+			if not gripper_on:
+				for obj_name in remaining_obj:
+					obj=detection_wire_kinect.InValue[obj_name]
+					if obj.detected:
+						pick(obj)
+						obj_grabbed=obj
+						action_performed=True
+						remaining_obj.remove(obj_name)
+						break
+
+		if gripper_on:
+			slot=detection_wire.InValue[obj_grabbed.name[0]+'_f']
+			#check slot is available and ready to drop
+			if slot.detected:
+				try:
+					place(obj_grabbed,obj_grabbed.name[0]+'_f')
+					action_performed=True
+				#if out of workspace
+				except ValueError:
+					pass
+				except UnboundLocalError:
+					pass
+				except:
+					traceback.print_exc()
+
+
+
+if __name__ == '__main__':
+	main()
---
+++ planner_service.py
@@ -0,0 +1,451 @@
+#!/usr/bin/env python3
+import tesseract
+from tesseract_viewer import TesseractViewer
+import os, re, copy
+import RobotRaconteur as RR
+RRN=RR.RobotRaconteurNode.s
+import numpy as np
+from qpsolvers import solve_qp
+import yaml, time, traceback, threading, sys
+sys.path.append('../')
+from gazebo_model_resource_locator import GazeboModelResourceLocator
+sys.path.append('../../toolbox')
+from abb_ik import inv as inv_abb
+from sawyer_ik import inv as inv_sawyer
+from ur_ik import inv as inv_ur
+from staubli_ik import inv as inv_staubli
+
+from general_robotics_toolbox import *    
+
+
+#convert 4x4 H matrix to 3x3 H matrix and inverse for mapping obj to robot frame
+def H42H3(H):
+	H3=np.linalg.inv(H[:2,:2])
+	H3=np.hstack((H3,-np.dot(H3,np.array([[H[0][-1]],[H[1][-1]]]))))
+	H3=np.vstack((H3,np.array([0,0,1])))
+	return H3
+
+class create_impl(object):
+	def __init__(self):
+		self._lock=threading.RLock()
+		self._running=False
+		#load calibration parameters
+		with open('calibration/Sawyer.yaml') as file:
+			H_Sawyer 	= np.array(yaml.load(file)['H'],dtype=np.float64)
+		with open('calibration/ur.yaml') as file:
+			H_UR 		= np.array(yaml.load(file)['H'],dtype=np.float64)
+		with open('calibration/abb.yaml') as file:
+			H_ABB 	= np.array(yaml.load(file)['H'],dtype=np.float64)
+
+		self.H_UR=H42H3(H_UR)
+		self.H_Sawyer=H42H3(H_Sawyer)
+		self.H_ABB=H42H3(H_ABB)
+		
+
+		self.distance_report=RRN.GetStructureType("edu.rpi.robotics.distance.distance_report")
+		self.dict={'ur':0,'sawyer':1,'abb':2}
+		self.distance_report_dict={}
+		for robot_name,robot_idx in self.dict.items():
+			self.distance_report_dict[robot_name]=self.distance_report()
+
+
+		#form H into RR transformation struct
+		self.transformations={}
+		self.transformation=RRN.GetStructureType("edu.rpi.robotics.distance.transformation")
+
+		transformation1=self.transformation()
+		transformation1.name="UR"
+		transformation1.row=len(self.H_UR)
+		transformation1.column=len(self.H_UR[0])
+		transformation1.H=np.float16(self.H_UR).flatten().tolist()
+		self.transformations['ur']=transformation1
+
+		transformation2=self.transformation()
+		transformation2.name="Sawyer"
+		transformation2.row=len(self.H_Sawyer)
+		transformation2.column=len(self.H_Sawyer[0])
+		transformation2.H=np.float16(self.H_Sawyer).flatten().tolist()
+		self.transformations['sawyer']=transformation2
+
+		transformation3=self.transformation()
+		transformation3.name="ABB"
+		transformation3.row=len(self.H_ABB)
+		transformation3.column=len(self.H_ABB[0])
+		transformation3.H=np.float16(self.H_ABB).flatten().tolist()
+		self.transformations['abb']=transformation3
+
+		
+		
+
+		#Connect to robot service
+		with open('client_yaml/client_ur.yaml') as file:
+			self.url_ur= yaml.load(file)['url']
+		with open('client_yaml/client_sawyer.yaml') as file:
+			self.url_sawyer= yaml.load(file)['url']
+		with open('client_yaml/client_abb.yaml') as file:
+			self.url_abb= yaml.load(file)['url']
+
+		self.ur_sub=RRN.SubscribeService(self.url_ur)
+		self.sawyer_sub=RRN.SubscribeService(self.url_sawyer)
+		self.abb_sub=RRN.SubscribeService(self.url_abb)
+
+		self.ur_sub.ClientConnectFailed += self.connect_failed
+		self.sawyer_sub.ClientConnectFailed += self.connect_failed
+		self.abb_sub.ClientConnectFailed += self.connect_failed
+
+		UR_state=self.ur_sub.SubscribeWire("robot_state")
+		Sawyer_state=self.sawyer_sub.SubscribeWire("robot_state")
+		ABB_state=self.abb_sub.SubscribeWire("robot_state")
+
+
+		#link and joint names in urdf
+		Sawyer_joint_names=["right_j0","right_j1","right_j2","right_j3","right_j4","right_j5","right_j6"]
+		UR_joint_names=["shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint"]
+		Sawyer_link_names=["right_l0","right_l1","right_l2","right_l3","right_l4","right_l5","right_l6","right_l1_2","right_l2_2","right_l4_2","right_hand"]
+		UR_link_names=['UR_base_link',"shoulder_link","upper_arm_link","forearm_link","wrist_1_link","wrist_2_link","wrist_3_link","UR_link_7"]
+		ABB_joint_names=['ABB1200_joint_1','ABB1200_joint_2','ABB1200_joint_3','ABB1200_joint_4','ABB1200_joint_5','ABB1200_joint_6']
+		ABB_link_names=['ABB1200_base_link','ABB1200_link_1','ABB1200_link_2','ABB1200_link_3','ABB1200_link_4','ABB1200_link_5','ABB1200_link_6','ABB1200_link_7']
+	
+		self.robot_state_list=[UR_state,Sawyer_state,ABB_state]
+		self.robot_link_list=[UR_link_names,Sawyer_link_names,ABB_link_names]
+		self.robot_joint_list=[UR_joint_names,Sawyer_joint_names,ABB_joint_names]
+		self.num_robot=len(self.robot_state_list)
+
+		######tesseract environment setup:
+
+		with open("urdf/combined.urdf",'r') as f:
+			combined_urdf = f.read()
+		with open("urdf/combined.srdf",'r') as f:
+			combined_srdf = f.read()
+
+		t = tesseract.Tesseract()
+		t.init(combined_urdf, combined_srdf, GazeboModelResourceLocator())
+		self.t_env = t.getEnvironment()
+		#update robot poses based on calibration file
+		self.t_env.changeJointOrigin("ur_pose", H_UR)
+		self.t_env.changeJointOrigin("sawyer_pose", H_Sawyer)
+		self.t_env.changeJointOrigin("abb_pose", H_ABB)
+
+		contact_distance=0.2
+		monitored_link_names = self.t_env.getLinkNames()
+		self.manager = self.t_env.getDiscreteContactManager()
+		self.manager.setActiveCollisionObjects(monitored_link_names)
+		self.manager.setContactDistanceThreshold(contact_distance)
+		# viewer update
+		self.viewer = TesseractViewer()
+		self.viewer.update_environment(self.t_env, [0,0,0])
+		self.viewer.start_serve_background()
+
+
+		self.robot_def_dict={'ur':Robot(np.transpose(np.array(UR.robot_info.chains[0].H.tolist())),np.transpose(np.array(UR.robot_info.chains[0].P.tolist())),np.zeros(len(UR.robot_info.joint_info))),
+		'sawyer':Robot(np.transpose(np.array(Sawyer.robot_info.chains[0].H.tolist())),np.transpose(np.array(Sawyer.robot_info.chains[0].P.tolist())),np.zeros(len(Sawyer.robot_info.joint_info))),
+		'abb':Robot(np.transpose(np.array(ABB.robot_info.chains[0].H.tolist())),np.transpose(np.array(ABB.robot_info.chains[0].P.tolist())),np.zeros(len(ABB.robot_info.joint_info))),
+		'staubli':Robot(np.transpose(np.array(tx60.robot_info.chains[0].H.tolist())),np.transpose(np.array(tx60.robot_info.chains[0].P.tolist())),np.zeros(len(tx60.robot_info.joint_info)))
+		}
+
+
+		#trajectories
+		self.steps=400
+		self.plan_time=0.4
+		self.trajectory={'ur':np.zeros((self.steps,7)),'sawyer':np.zeros((self.steps,8)),'abb':np.zeros((self.steps,7)),'staubli':np.zeros((self.steps,7))}
+		self.traj_joint_names={'ur':['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'],
+		'sawyer':['right_j0', 'right_j1', 'right_j2', 'right_j3', 'right_j4', 'right_j5', 'right_j6'],
+		'abb':['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6'],
+		'staubli':['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6']
+		}
+		self.time_step=0.02
+		#initialize static trajectories
+		for key, value in self.trajectory.items():
+			for i in range(self.steps):
+				try:
+					value[i]=np.append([0],self.robot_state_list[self.dict[key]].InValue.joint_position)
+				except:
+					traceback.print_exc()
+					value[i]=np.append([0],[0,0,0,0,0,0])
+		self.inv={'ur':inv_ur,'sawyer':inv_sawyer,'abb':inv_abb,'staubli':inv_staubli}
+		self.joint_names_traj={'ur':inv_ur,'sawyer':inv_sawyer,'abb':inv_abb,'staubli':inv_staubli}
+
+		
+
+		#register service constant
+		self.JointTrajectoryWaypoint = RRN.GetStructureType("com.robotraconteur.robotics.trajectory.JointTrajectoryWaypoint")
+		self.JointTrajectory = RRN.GetStructureType("com.robotraconteur.robotics.trajectory.JointTrajectory")
+
+	#connection failed callback
+	def connect_failed(self, s, client_id, url, err):
+	    print ("Client connect failed: " + str(client_id.NodeID) + " url: " + str(url) + " error: " + str(err))
+	    if url==self.url_ur:
+	    	self.ur_sub=RRN.SubscribeService(self.url_ur)
+	    elif url==self.url_sawyer:
+	    	self.sawyer_sub=RRN.SubscribeService(self.url_sawyer)
+
+	def Sawyer_link(self,J2C):
+		if J2C+1==7:
+			return 1
+		elif J2C+1==8:
+			return 2
+		elif J2C+1==9:
+			return 4
+		elif J2C+1==10:
+			return 7
+		else:
+			return J2C
+
+	def viewer_update(self):
+		while self._running:
+			with self._lock:
+				for i in range(self.num_robot):
+					robot_joints=self.robot_state_list[i].InValue.joint_position
+					self.t_env.setState(self.robot_joint_list[i], robot_joints)
+				self.viewer.update_environment(self.t_env, [0,0,0])
+
+	def distance_check_global(self,robot_name, joints_list):
+		with self._lock:
+			robot_idx=self.dict[robot_name]
+			distance_report=RRN.GetStructureType("edu.rpi.robotics.distance.distance_report")
+			distance_report1=distance_report()
+
+			for i in range(self.num_robot):
+				robot_joints=joints_list[i]
+				self.t_env.setState(self.robot_joint_list[i], robot_joints)
+
+			env_state = self.t_env.getCurrentState()
+			self.manager.setCollisionObjectsTransform(env_state.link_transforms)
+			contacts = self.manager.contactTest(2)
+
+			contact_vector = tesseract.flattenResults(contacts)
+
+			distances = np.array([c.distance for c in contact_vector])
+			nearest_points=np.array([c.nearest_points for c in contact_vector])
+			names = np.array([c.link_names for c in contact_vector])
+			# nearest_index=np.argmin(distances)	
+
+			min_distance=9
+			min_index=-1
+			Closest_Pt=[0.,0.,0.]
+			Closest_Pt_env=[0.,0.,0.]
+			#initialize
+			distance_report1.Closest_Pt=Closest_Pt
+			distance_report1.Closest_Pt_env=Closest_Pt_env
+
+			for i in range(len(distances)):
+
+				#only 1 in 2 collision "objects"
+				if (names[i][0] in self.robot_link_list[robot_idx] or names[i][1] in self.robot_link_list[robot_idx]) and distances[i]<min_distance and not (names[i][0] in self.robot_link_list[robot_idx] and names[i][1] in self.robot_link_list[robot_idx]):
+					min_distance=distances[i]
+					min_index=i
+
+
+			J2C=0
+			if (min_index!=-1):
+				if names[min_index][0] in self.robot_link_list[robot_idx] and names[min_index][1] in self.robot_link_list[robot_idx]:
+					stop=1
+
+				elif names[min_index][0] in self.robot_link_list[robot_idx]:
+					J2C=self.robot_link_list[robot_idx].index(names[min_index][0])-1
+					Closest_Pt=nearest_points[min_index][0]
+					Closest_Pt_env=nearest_points[min_index][1]
+
+				elif names[min_index][1] in self.robot_link_list[robot_idx]:
+					J2C=self.robot_link_list[robot_idx].index(names[min_index][1])-1
+					Closest_Pt=nearest_points[min_index][1]
+					Closest_Pt_env=nearest_points[min_index][0]
+
+
+				if robot_idx==1:
+					J2C=self.Sawyer_link(J2C)
+
+				
+				distance_report1.Closest_Pt=np.float16(Closest_Pt).flatten().tolist()
+				distance_report1.Closest_Pt_env=np.float16(Closest_Pt_env).flatten().tolist()
+				distance_report1.min_distance=np.float16(distances[min_index])
+				distance_report1.J2C=(J2C if J2C>=0 else 0)		
+				
+				
+				return distance_report1
+
+			return distance_report1
+
+	def plan(self,robot_name,speed,pd,Rd, obj_vel, capture_time):            #start and end configuration in joint space
+
+		plan_start_time=time.time()
+		#update other robot static trajectories
+		for key, value in self.trajectory.items():
+			if value[0][0]==0:
+				try:
+					value[:]=np.append([0],self.robot_state_list[self.dict[key]].InValue.joint_position)
+				except:
+					value[:]=[0]*7
+
+
+		Rd=Rd.reshape((3,3))
+		start_time=time.time()+self.plan_time
+		distance_threshold=0.1
+		joint_threshold=0.1
+
+		#get joint info in future 
+		other_robot_trajectory_start_idx={'ur':self.steps-1,'sawyer':self.steps-1,'abb':self.steps-1,'staubli':self.steps-1}
+
+		for key, value in self.trajectory.items():
+			if key==robot_name:
+				continue
+			if value[0][0]!=0:
+				other_robot_trajectory_start_idx[key] = (np.abs(value[:,0] - start_time)).argmin()
+
+		#parameter setup
+		n= len(self.robot_joint_list[self.dict[robot_name]])
+
+		#calc desired joint angles
+		q_des=self.inv[robot_name](pd,Rd).reshape(n)
+
+
+		w=1                  #set the weight between orientation and position
+		Kq=.01*np.eye(n)    #small value to make sure positive definite
+		Kp=np.eye(3)
+		KR=np.eye(3)        #gains for position and orientation error
+		step=0
+
+		EP=[1,1,1]
+		q_cur=self.robot_state_list[self.dict[robot_name]].InValue.joint_position
+
+		#initialize trajectory
+		self.trajectory[robot_name][step]=np.append(0.,q_cur)
+		waypoints = []
+		wp = self.JointTrajectoryWaypoint()
+		wp.joint_position = copy.deepcopy(q_cur)
+		wp.time_from_start = 0.
+		waypoints.append(wp)
+
+
+		while(np.linalg.norm(q_des[:-1]-q_cur[:-1])>joint_threshold):
+			if step>self.steps:
+				raise UnboundLocalError("Unplannable")
+				return
+			if np.linalg.norm(obj_vel)!=0:
+				p_d=(pd+obj_vel*(time.time()-capture_time))
+
+				q_des=self.inv[robot_name](p_d,Rd).reshape(n)
+			else:
+				p_d=pd
+			
+
+		#     get current H and J
+			robot_pose=self.robot_state_list[self.dict[robot_name]].InValue.kin_chain_tcp[0]
+			R_cur = q2R(np.array(robot_pose['orientation'].tolist()))
+
+			p_cur=np.array(robot_pose['position'].tolist())
+
+			J=robotjacobian(self.robot_def_dict[robot_name],q_cur)        #calculate current Jacobian
+			Jp=J[3:,:]
+			JR=J[:3,:]                              #decompose to position and orientation Jacobian
+
+			ER=np.dot(R_cur,np.transpose(Rd))
+			EP=p_cur-p_d                             #error in position and orientation
+			#update future joint to distance checking
+
+			joints_list=[self.trajectory['ur'][np.amin([step+other_robot_trajectory_start_idx['ur'],self.steps-1])][1:],self.trajectory['sawyer'][np.amin([step+other_robot_trajectory_start_idx['sawyer'],self.steps-1])][1:],
+			self.trajectory['abb'][np.amin([step+other_robot_trajectory_start_idx['abb'],self.steps-1])][1:],self.trajectory['staubli'][np.amin([step+other_robot_trajectory_start_idx['staubli'],self.steps-1])][1:]]
+			#update self joint position
+			joints_list[self.dict[robot_name]]=q_cur
+			distance_report=self.distance_check_global(robot_name,joints_list)
+
+
+			Closest_Pt=distance_report.Closest_Pt
+			Closest_Pt_env=distance_report.Closest_Pt_env
+			dist=distance_report.min_distance
+			J2C=distance_report.J2C
+
+			if (Closest_Pt[0]!=0. and dist<distance_threshold):  
+
+				print("qp triggering ",dist ) 
+				Closest_Pt[:2]=np.dot(self.H_robot[robot_name],np.append(Closest_Pt[:2],1))[:2]
+				Closest_Pt_env[:2]=np.dot(self.H_robot[robot_name],np.append(Closest_Pt_env[:2],1))[:2] 
+
+				k,theta = R2rot(ER)             #decompose ER to (k,theta) pair
+
+			#   set up s for different norm for ER
+
+				s=np.sin(theta/2)*k         #eR2
+				vd=-np.dot(Kp,EP)
+				wd=-np.dot(KR,s)          
+				H=np.dot(np.transpose(Jp),Jp)+Kq+w*np.dot(np.transpose(JR),JR)
+				H=(H+np.transpose(H))/2
+
+				f=-np.dot(np.transpose(Jp),vd)-w*np.dot(np.transpose(JR),wd)               #setup quadprog parameters
+
+
+				dx = Closest_Pt_env[0] - Closest_Pt[0]
+				dy = Closest_Pt_env[1] - Closest_Pt[1]
+				dz = Closest_Pt_env[2] - Closest_Pt[2]
+
+				# derivative of dist w.r.t time
+				der = np.array([dx/dist, dy/dist, dz/dist])
+				J_Collision=np.hstack((J[3:,:J2C],np.zeros((3,n-J2C))))
+
+				A=np.dot(der.reshape((1,3)),J_Collision)
+				
+				b=np.array([dist - 0.1])
+
+				try:
+					qdot=1.*normalize_dq(solve_qp(H, f,A,b))
+					
+				except:
+					traceback.print_exc()
+
+			else:
+				if np.linalg.norm(q_des-q_cur)<0.5 or step*self.time_step<0.1:
+					qdot=normalize_dq(q_des-q_cur)
+				else:
+					qdot=speed*normalize_dq(q_des-q_cur)
+			#update q_cur
+			q_cur+=qdot*self.time_step
+			step+=1
+			self.trajectory[robot_name][step]=np.append(self.time_step*step,q_cur)
+			#RR trajectory formation
+			
+			wp = self.JointTrajectoryWaypoint()
+			wp.joint_position = copy.deepcopy(q_cur)
+			wp.time_from_start = step*self.time_step
+			waypoints.append(wp)
+
+		#populate all after the goal configuration
+		self.trajectory[robot_name][step:]=np.append(self.time_step*step,q_cur)
+
+		traj = self.JointTrajectory()
+		traj.joint_names = self.traj_joint_names[robot_name]
+		traj.waypoints = waypoints
+
+		#dynamic planning time
+		self.plan_time=time.time()-plan_start_time+0.12
+		
+		#estimate of time
+		self.trajectory[robot_name][:,0]+=time.time()
+
+		return traj
+
+with RR.ServerNodeSetup("Distance_Service", 25522) as node_setup:
+	cwd = os.getcwd()
+	#register robot service definition
+	directory='/home/iamnotedible/catkin_ws/src/robotraconteur_companion/robdef/group1/'
+	os.chdir(directory)
+	RRN.RegisterServiceTypesFromFiles(['com.robotraconteur.robotics.trajectory.robdef'],True)
+	os.chdir(cwd)
+
+	#register service file and service
+	RRN.RegisterServiceTypeFromFile("robdef/edu.rpi.robotics.distance")
+	distance_inst=create_impl()				#create obj
+
+	RRN.RegisterService("Environment","edu.rpi.robotics.distance.env",distance_inst)
+	print("distance service started")
+
+	distance_inst.plan('abb',[0.5,0.5,0.5],np.array([1,0,0,0,1,0,0,0,1]),[0,0,0],0)
+	input("Press enter to quit")
+
+
+
+
+
+
+
+
+
