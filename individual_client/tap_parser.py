9645f4653ab6ff356ef01cd7a663f46c8d047477
hehonglu123 <hehonglu123@126.com>
2020-12-26 19:29:48 -0500

    parser

--- individual_client/tap_parser.py
+++ individual_client/tap_parser.py
@@ -1,12 +1,21 @@
 import RobotRaconteur as RR
 RRN=RR.RobotRaconteurNode.s
 import RobotRaconteurCompanion as RRC
-import traceback,os
+import numpy as np
+import traceback, os, json
 
 RRC.RegisterStdRobDefServiceTypes(RRN)
 
 
 fname = r"my_robot_1-2020-12-24T21-33-51.robtap"
+
+out=[]
+
+
+def parse_structure(val):
+    temp_js={}
+    return temp_js
+
 
 
 with open(fname,"rb") as f:
@@ -16,15 +25,28 @@ with open(fname,"rb") as f:
         if m is None:
             break
         entry = m.entries[0]
-        print (entry.MemberName)
-        if (entry.MemberName == "robot_state"):
-            for i in range(len(entry.elements)):
-                el = entry.elements[i]
-                print(el.ElementName)
-                if el.ElementName == "packet":
-                    val = reader.UnpackMessageElement(el)
-                    print(val.data.joint_position_command)
-                # if el.ElementName == "packettime":
-                #     val = reader.UnpackMessageElement(el)
-                #     print(val.data)
-        
+        # print (entry.MemberName)
+        temp={entry.MemberName:[]}
+
+        for i in range(len(entry.elements)):
+            el = entry.elements[i]
+            temp_el={el.ElementName:None}
+            if el.ElementName=='packettime':
+                continue
+
+            val = reader.UnpackMessageElement(el)
+            if el.ElementType == 101:
+                temp_el[el.ElementName]=parse_structure(val)
+            else:
+                if isinstance(val.data,np.ndarray):
+                    temp_el[el.ElementName]=val.data.tolist()
+                else:
+                    temp_el[el.ElementName]=val.data
+                
+            temp[entry.MemberName].append(temp_el)
+        out.append(temp)
+
+fname=fname[:-6]+'json'
+
+with open(fname,'w') as f:
+    json.dump(out,f,indent=2)
